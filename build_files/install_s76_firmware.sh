6731709087f08791391dbf6658444b8d4d2e0579
Cory Adkins <adkins.cory@gmail.com>
2025-07-16 23:37:01 -0400

    add system76-firmware

---
+++ build_files/install_s76_firmware.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -oeux pipefail
+# install system76 support packages
+dnf copr enable szydell/system76
+dnf install -y /
+  system76-driver /
+  system76-dkms /
+  system76-power /
+  system76-firmware /
+  firmware-manager /
+  system76-acpi-dkms
+dnf copr disable szydell/system76
+
+
