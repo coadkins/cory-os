74648160c7e418c4130c9d7332e7f9236f327d5e
Cory Adkins <adkins.cory@gmail.com>
2025-07-17 12:22:11 -0400

    fix system76-power

--- build_files/remove_bluefin_packages.sh
+++ build_files/remove_bluefin_packages.sh
@@ -3,3 +3,8 @@ set -ouex pipefail
 # remove packages installed with dnf
 dnf remove -y \
 	code 
+
+# disable power profiles daemon in favor of system76 one
+systemctl stop power-profiles-daemon.service
+systemctl disable power-profiles-daemon.service
+systemctl mask power-profiles-daemon.service
