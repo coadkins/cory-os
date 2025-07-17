81c4922c3f72e72bd27b7aa7c22c13cdc744b0ef
Cory Adkins <adkins.cory@gmail.com>
2025-07-17 00:56:07 -0400

    fixup! add system76-firmware

--- build_files/install_s76_firmware.sh
+++ build_files/install_s76_firmware.sh
@@ -2,12 +2,12 @@
 set -oeux pipefail
 # install system76 support packages
 dnf copr enable szydell/system76
-dnf install -y /
-  system76-driver /
-  system76-dkms /
-  system76-power /
-  system76-firmware /
-  firmware-manager /
+dnf install -y \
+  system76-driver \
+  system76-dkms \
+  system76-power \
+  system76-firmware \
+  firmware-manager \
   system76-acpi-dkms
 dnf copr disable szydell/system76
 
