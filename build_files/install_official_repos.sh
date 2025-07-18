d085b33cc4be7febaf0f427a5efac0758aa4652e
Cory Adkins <adkins.cory@gmail.com>
2025-07-18 15:45:16 -0400

    add emacs

--- build_files/install_official_repos.sh
+++ build_files/install_official_repos.sh
@@ -5,9 +5,10 @@ set -ouex pipefail
 echo "installing packages from official fedora repos"
 dnf5 install -y \
 	neovim \
+	emacs \
 	rsync \
 	make \
-        unixODBC-devel \
+    unixODBC-devel \
 	libcurl-devel \
 	openssl-devel
 
