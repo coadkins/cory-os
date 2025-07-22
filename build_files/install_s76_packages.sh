857915d99cb0298fd4ecbe8168ce9976af46e159
Cory Adkins <adkins.cory@gmail.com>
2025-07-22 10:41:21 -0400

    install s76 user space packages (power/firmware mgr)

--- build_files/install_s76_packages.sh
+++ build_files/install_s76_packages.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -oeux pipefail
+# freeze kernel packages just in case
+dnf5 versionlock add kernel kernel-devel kernel-devel-matched kernel-core kernel-modules kernel-modules-core kernel-modules-extra kernel-tools kernel-tools-libs
 # install system76 support packages
 dnf5 -y copr enable szydell/system76
 dnf install -y \
