6273d22c69244fda8c101dfd9ff74666ec8f7016
Cory Adkins <adkins.cory@gmail.com>
2025-07-23 12:43:01 -0400

    change paths to reflect mainline ublue naming

--- build_files/install_kernel_akmods.sh
+++ build_files/install_kernel_akmods.sh
@@ -2,16 +2,18 @@
 set -eoux pipefail
 # install newer kernel
 dnf5 -y install \
-    /var/tmp/kernel-rpms/kernel-[0-9]*.rpm \
-    /var/tmp/kernel-rpms/kernel-core-*.rpm \
-    /var/tmp/kernel-rpms/kernel-modules-*.rpm \
-    /var/tmp/kernel-rpms/kernel-tools-[0-9]*.rpm \
-    /var/tmp/kernel-rpms/kernel-tools-libs-[0-9]*.rpm \
-    /var/tmp/kernel-rpms/kernel-devel-*.rpm
-
+    /var/tmp/kernel-rpms/kernel*.rpm \
+    /var/tmp/kernel-rpms/kernel-core*.rpm \
+    /var/tmp/kernel-rpms/kernel-modules*.rpm \
+    /var/tmp/kernel-rpms/kernel-modules-core*.rpm \
+    /var/tmp/kernel-rpms/kernel-modules-extra*.rpm
 echo "::group::Executing install-kernel-akmods"
 trap 'echo "::endgroup::"' EXIT
 
+# install ublue support package
+dnf5 install /var/tmp/akmods-rpms/ublue-os/ublue-os-akmods*.rpm
+
+# install system76 drivers
 dnf5 -y copr enable ssweeny/system76-hwe
 
 dnf5 versionlock add kernel kernel-devel kernel-devel-matched kernel-core kernel-modules kernel-modules-core kernel-modules-extra kernel-tools kernel-tools-libs
