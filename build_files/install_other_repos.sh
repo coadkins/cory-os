009ec284549601aec5c3f067cf1dc3fe2739965d
Cory Adkins <adkins.cory@gmail.com>
2025-08-01 21:41:00 -0400

    Revert "add ghostty as appimage"
    This reverts commit ab0842f427dc3de52264bc363a7ddb28d13c0e81.

--- build_files/install_other_repos.sh
+++ build_files/install_other_repos.sh
@@ -7,16 +7,4 @@ echo "installing vscodium"
 dnf5 install -y codium
 echo "disabling vscodium repo"
 dnf5 config-manager setopt gitlab.com_paulcarroty_vscodium_repo.enabled=0
-# add ghostty as AppImage
-# Ghostty as appimage :(
-while [[ -z "${GHOSTTY:-}" || "${GHOSTTY:-}" == "null" ]]; do
-    GHOSTTY="$(curl -L https://api.github.com/repos/pkgforge-dev/ghostty-appimage/releases/latest | jq -r '.assets[] | select(.name| test("Ghostty-[0-9].*-x86_64.AppImage$")).browser_download_url')" || (true && sleep 5)
-done
-curl --retry 3 -Lo /tmp/ghostty.appimage "$GHOSTTY"
-cd /tmp/
-chmod +x /tmp/ghostty.appimage
-/tmp/ghostty.appimage --appimage-extract
-mkdir -p /usr/share/icons/hicolor/256x256/apps/
-cp /tmp/AppDir/"$(readlink /tmp/squashfs-root/*.png)" /usr/share/icons/hicolor/256x256/apps/
-cp /tmp/AppDir/"$(readlink /tmp/squashfs-root/*.desktop)" /usr/share/applications/
-install -m 0755 /tmp/ghostty.appimage /usr/bin/ghostty
+
