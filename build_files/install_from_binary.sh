93bc411f2738a29928777dafcc416e108b273245
Cory Adkins <adkins.cory@gmail.com>
2025-08-19 21:18:55 -0400

    remove min web browser

--- build_files/install_from_binary.sh
+++ build_files/install_from_binary.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 set -oeux pipefail
-
-# min browser
-wget https://github.com/minbrowser/min/releases/download/v1.35.1/min-1.35.1-x86_64.rpm -O /tmp/minbrowser.rpm
-dnf5 install -y /tmp/minbrowser.rpm
-
 # incus web UI
 wget https://pkgs.zabbly.com/incus/stable/pool/main/i/incus/incus-ui-canonical_6.15-ubuntu24.04-202508170325_amd64.deb -O /tmp/incus-ui-canonical.deb
 dpkg -x /tmp/incus-ui-canonical.deb /tmp/incus-ui/
