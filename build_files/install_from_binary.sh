c4e8075b09d018e96b7ce5d28928d946c08d0fdf
Cory Adkins <adkins.cory@gmail.com>
2025-09-13 17:45:05 -0400

    fix address for incus web ui package

--- build_files/install_from_binary.sh
+++ build_files/install_from_binary.sh
@@ -2,7 +2,7 @@
 
 set -oeux pipefail
 # incus web UI
-wget https://pkgs.zabbly.com/incus/stable/pool/main/i/incus/incus-ui-canonical_6.15-ubuntu24.04-202508170325_amd64.deb -O /tmp/incus-ui-canonical.deb
+wget https://pkgs.zabbly.com/incus/stable/pool/main/i/incus/incus-ui-canonical_6.16-debian11-202509082238_amd64.deb -O /tmp/incus-ui-canonical.deb
 dpkg -x /tmp/incus-ui-canonical.deb /tmp/incus-ui/
 mkdir -p /usr/share/incus/
 cp -r /tmp/incus-ui/opt/incus/. /usr/share/incus/
